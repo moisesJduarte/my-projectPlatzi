944552b6cdddf7fde811d5dbdb419f5b9b1b0bbb
Moises Duarte <gladmd19@gmail.com>
2023-04-07 05:59:12 -0400

    building app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
 import { Component } from '@angular/core';
-
+import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-projectPlatzi';
+  name = 'my-projectPlatzi';
+  age = 18;
+  img = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  person = {
+    name: 'Nicolas',
+    age: 18,
+    avatar: 'https://source.unsplash.com/random'
+  }
+
+  names: string[] = ['nicolas', 'juli', 'santi'];
+  newName = '';
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+  
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+
+  
+  
+  addName() {
+  this.names.push(this.newName);
+  this.newName = '';
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1)
+  }
 }
